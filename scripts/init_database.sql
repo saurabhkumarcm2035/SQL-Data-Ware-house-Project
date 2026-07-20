84d55a6ecea90974a6b40ef6da271d6a1b00b353
saurabhkumarcm2035 <saurabhyadavsy986@gmail.com>
2026-07-20 17:13:00 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,8 @@
+-- ============================================
+-- Create and initialize the Data Warehouse
+-- ============================================
+
+-- Create the database if it doesn't already exist
+CREATE DATABASE IF NOT EXISTS datawarehouse;
+
+USE datawarehouse;
